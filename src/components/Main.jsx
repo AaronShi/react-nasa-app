a2fe7cdcb8866782212af9a13785598b177c6a8e
AaronS <aaron.shiyang@gmail.com>
2025-04-01 10:59:44 -0700

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,33 @@ import React from "react";
 
 function Main(props) {
   const { data } = props;
+  let image_type = data.media_type === "video";
 
-  return (
+  return { image_type } ? (
     <div className="imgContainer">
-      <img className="bgImage" src={data?.hdurl} alt={data?.title || "bg-img"} />
+      <iframe
+        width="100%"
+        height="90%"
+        src={data.url}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; 
+        autoplay; 
+        clipboard-write; 
+        encrypted-media; 
+        gyroscope; 
+        picture-in-picture; 
+        web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+  ) : (
+    <div className="imgContainer">
+      <img
+        className="bgImage"
+        src={data?.hdurl}
+        alt={data?.title || "bg-img"}
+      />
     </div>
   );
 }
