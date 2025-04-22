080f942ac839522a891b15418e2b538567b6b9aa
AaronS <aaron.shiyang@gmail.com>
2025-04-21 21:59:38 -0700

    bug fix for youtube video

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,33 +2,34 @@ import React from "react";
 
 function Main(props) {
   const { data } = props;
-  let image_type = data.media_type === "video";
+  let image_type = data.media_type === "image";
 
   return { image_type } ? (
-    <div className="imgContainer">
-      <iframe
-        width="100%"
-        height="90%"
-        src={data.url}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; 
-        autoplay; 
-        clipboard-write; 
-        encrypted-media; 
-        gyroscope; 
-        picture-in-picture; 
-        web-share"
-        allowfullscreen
-      ></iframe>
-    </div>
-  ) : (
     <div className="imgContainer">
       <img
         className="bgImage"
         src={data?.hdurl}
         alt={data?.title || "bg-img"}
       />
+    </div>
+  ) : (
+    <div className="imgContainer">
+      <iframe
+        width="100%"
+        height="90%"
+        src={data.url}
+        // src={data.url + "&embedded=true"}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture;
+        web-share"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
